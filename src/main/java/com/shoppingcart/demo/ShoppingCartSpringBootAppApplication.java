5e892e588d8e2187f2691e6aae53a1faf462e976
S S <368221@cognizant.com>
2019-07-05 12:11:52 +0530

    modified ui and code

--- src/main/java/com/shoppingcart/demo/ShoppingCartSpringBootAppApplication.java
+++ src/main/java/com/shoppingcart/demo/ShoppingCartSpringBootAppApplication.java
@@ -1,7 +1,13 @@
 package com.shoppingcart.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.shoppingcart.demo.model.Product;
+import com.shoppingcart.demo.service.ProductService;
 
 @SpringBootApplication
 public class ShoppingCartSpringBootAppApplication {
@@ -10,4 +16,18 @@ public class ShoppingCartSpringBootAppApplication {
 		SpringApplication.run(ShoppingCartSpringBootAppApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner runner(ProductService productService) {
+		return args -> {
+			productService.save(new Product(1L, "TV Set", 300.00, "http://uhfnocturne.com/wp-content/uploads/2017/09/aj.jpg"));
+			productService.save(new Product(2L, "Game Console", 200.00, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWuxnRzKBxvYij-dFDcN_Iukub3cMLZ8BaYFDn3FdoQhe1bzn2OQ"));
+			productService.save(new Product(3L, "Sofa", 100.00, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT69jwGxxDVqH051qPD52ETRdsUehfk42OxzpknOL4VztIn4K6d"));
+			productService.save(new Product(4L, "Icecream", 5.00, "https://images.pexels.com/photos/1362534/pexels-photo-1362534.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"));
+			productService.save(new Product(5L, "Beer", 3.00, "https://www.ultraliquors.co.za/images/cooldrinks/beer-at-ultraliquors.jpg"));
+			productService.save(new Product(6L, "Phone", 500.00, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-SoYatNte-P5rgz9qyFTSCM2t0RBIrLrmWckENY5f3_GKher4"));
+			productService.save(new Product(7L, "Watch", 30.00, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzF66AY63iQHD83VlPoGGFnuL0LYFdXqFrS_t_KLFC0ipjYJGCRg"));
+			productService.save(new Product(8L, "Shoe", 15.00, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiH_zybxAk5-cCTYgodJttedRv7Y_V5Gb2qX2OqOfWIlWZQfC5"));
+			productService.save(new Product(9L, "Clock", 50.00, "https://cdn.shopify.com/s/files/1/1256/7113/products/04June2019-CliMET-6_600.jpg?v=1560163205"));
+		};
+	}
 }
