96e14eb1f2485561884f1d8652cc4c8cd89db258
S S <368221@cognizant.com>
2019-07-08 14:30:57 +0530

    login added new fix 11

--- src/main/frontend/src/app/ecommerce/services/authentication-service.service.ts
+++ src/main/frontend/src/app/ecommerce/services/authentication-service.service.ts
@@ -2,17 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-
+import { AlertService } from '../services/alert-service.service';
 import { User } from '../models/user';
 
 @Injectable()
 export class AuthenticationService {
     private currentUserSubject: BehaviorSubject<User>;
     public currentUser: Observable<User>;
-
+    private alertService: AlertService
     constructor(private http: HttpClient) {
         this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
         this.currentUser = this.currentUserSubject.asObservable();
+        
     }
 
     public get currentUserValue(): User {
@@ -22,6 +23,7 @@ export class AuthenticationService {
     login(username, password) {
         return this.http.post<any>(`http://myspringapp-springbootdemoapps.apps.cdeocp.com/users/authenticate`, { username, password })
             .pipe(map(user => {
+                this.alertService.success('Login successful', true);
                 // store user details and jwt token in local storage to keep user logged in between page refreshes
                 localStorage.setItem('currentUser', JSON.stringify(user));
                 this.currentUserSubject.next(user);
